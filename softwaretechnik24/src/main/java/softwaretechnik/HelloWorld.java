75dfb0104805211d75ca644479cfa0b924996dc6
CGoh <nina.gierke@stud.th-luebeck.de>
2024-10-31 13:51:11 +0100

    New Tests sayHello

--- softwaretechnik24/src/main/java/softwaretechnik/HelloWorld.java
+++ softwaretechnik24/src/main/java/softwaretechnik/HelloWorld.java
@@ -4,8 +4,14 @@ package softwaretechnik;
  * Hello world!
  *
  */
-public class App 
+public class HelloWorld 
 {
+
+public String sayHello(String name) {
+    System.out.println( "Hello" + name );
+    return "Hello" + name;
+}
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- softwaretechnik24/src/test/java/softwaretechnik/AppTest.java
+++ softwaretechnik24/src/test/java/softwaretechnik/AppTest.java
@@ -1,5 +1,6 @@
 package softwaretechnik;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,12 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHello("Steve");
+        assertEquals("Hello Steve!", result); // Erwartetes Ergebnis
+    }
+
 }
