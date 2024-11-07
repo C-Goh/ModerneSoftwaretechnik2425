b7c2edf71e31650c09becc577e7ba27aca0f6e44
Aziz Kilic <azizkilic942@gmail.com>
2024-11-07 17:59:11 +0100

    Add SQLite Manager

--- softwaretechnik24/src/main/java/softwaretechnik/App.java
+++ softwaretechnik24/src/main/java/softwaretechnik/App.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import softwaretechnik.database.SQLiteManager;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
+        SQLiteManager.initializeDatabase();
+
         final Label label = new Label("Hello, JavaFX!"); // Violation: line length exceeds 80 characters
         final Scene scene = new Scene(label, 400, 200);
         primaryStage.setScene(scene);
