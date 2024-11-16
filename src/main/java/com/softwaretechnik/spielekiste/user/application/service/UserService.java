82e7825af3cdb381d5d648d00d49f5e0ae460fdc
Aziz Kilic <azizkilic942@gmail.com>
2024-11-16 14:23:09 +0100

    Change javadoc dependency

--- src/main/java/com/softwaretechnik/spielekiste/user/application/service/UserService.java
+++ src/main/java/com/softwaretechnik/spielekiste/user/application/service/UserService.java
@@ -6,17 +6,31 @@ import com.softwaretechnik.spielekiste.domain.user.service.UserDomainService;
 
 import java.util.List;
 
+/**
+ * Service class for managing users.
+ */
 public class UserService {
 
     private final UserRepository userRepository;
 
     private final UserDomainService userDomainService;
 
+    /**
+     * Constructs a new UserService with the given repositories.
+     *
+     * @param userRepository the user repository
+     * @param userDomainService the user domain service
+     */
     public UserService(UserRepository userRepository, UserDomainService userDomainService) {
         this.userRepository = userRepository;
         this.userDomainService = userDomainService;
     }
 
+    /**
+     * Creates a new user.
+     *
+     * @param user the user entity
+     */
     public void createUser(UserEntity user) {
         userDomainService.validateUser(user);
         userRepository.createUser(user);
