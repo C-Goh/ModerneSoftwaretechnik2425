71ff08ffda6eb7c74a42c56fffc2d9afe6bb9e7f
Florin, Lina <lina.florin@stud.th-luebeck.de>
2024-12-16 13:57:00 +0100

    fix checkstyle and failed pipeline

--- src/main/java/com/softwaretechnik/spielekiste/ui/controller/PageLoader.java
+++ src/main/java/com/softwaretechnik/spielekiste/ui/controller/PageLoader.java
@@ -1,12 +1,12 @@
 package com.softwaretechnik.spielekiste.ui.controller;
 
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.event.Event;
 
 public class PageLoader {
 
@@ -27,11 +27,11 @@ public class PageLoader {
     public void loadStartPage(MouseEvent event) {
         try {
             // Lade die StartPage.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/StartPage.fxml"));
-            Parent root = loader.load();
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/StartPage.fxml"));
+            final Parent root = loader.load();
 
             // Aktuelle Bühne ermitteln
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            final Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             // Neue Szene setzen
             stage.setScene(new Scene(root, 800, 600));
@@ -61,11 +61,11 @@ public class PageLoader {
     public void loadBadgeOverviewPage(MouseEvent event) {
         try {
             // Lade die BadgeOverview-Seite (BadgeOverview.fxml)
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/BadgeOverview.fxml"));
-            Parent root = loader.load();
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/BadgeOverview.fxml"));
+            final Parent root = loader.load();
 
             // Aktuelle Bühne ermitteln
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            final Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             // Neue Szene setzen
             stage.setScene(new Scene(root, 800, 600));
@@ -78,11 +78,11 @@ public class PageLoader {
     public void loadGameOverviewPage(Event event) {
         try {
             // Lade die GameOverview-Seite (GameOverview.fxml)
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/GameOverview.fxml"));
-            Parent root = loader.load();
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/view/GameOverview.fxml"));
+            final Parent root = loader.load();
 
             // Aktuelle Bühne ermitteln
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            final Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             // Neue Szene setzen
             stage.setScene(new Scene(root, 800, 600));
